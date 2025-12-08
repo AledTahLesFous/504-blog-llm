1d522f5809585831a22c607af93e2dc16c69eb7b
Mairofrb <160046372+Mairofrb@users.noreply.github.com>
2025-12-08 19:23:46 +0100

    feat: routes update

--- site/routes/web.php
+++ site/routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NewsController;
 
-Route::get('/', function () {
-    return view('welcome');
+// Routes pour l'interface web
+Route::get('/', [NewsController::class, 'index'])->name('news.index');
+Route::get('/source/{source}', [NewsController::class, 'source'])->name('news.source');
+Route::get('/article/{id}', [NewsController::class, 'article'])->name('news.article');
+
+// Routes API JSON
+Route::prefix('api')->group(function () {
+    Route::get('/news', [NewsController::class, 'apiAll'])->name('api.news.all');
+    Route::get('/news/{source}', [NewsController::class, 'apiSource'])->name('api.news.source');
 });
