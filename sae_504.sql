3652444bf5b4f944eef3ac29eb12ce40411e58f9
AledTahLesFous <clement.muzard@etu.umontpellier.fr>
2025-12-08 15:57:18 +0100

    chore: db added dump data

--- sae_504.sql
+++ sae_504.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost
--- Généré le : lun. 08 déc. 2025 à 10:22
+-- Généré le : lun. 08 déc. 2025 à 14:55
 -- Version du serveur : 12.1.2-MariaDB
 -- Version de PHP : 8.5.0
 
@@ -28,10 +28,44 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `articles` (
-  `id` uuid NOT NULL DEFAULT uuid(),
-  `nom` varchar(255) NOT NULL,
-  `contenu` text NOT NULL
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `subtitle` varchar(255) DEFAULT NULL,
+  `content` text NOT NULL,
+  `published` tinyint(1) NOT NULL DEFAULT 0,
+  `published_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `articles`
+--
+
+INSERT INTO `articles` (`id`, `title`, `subtitle`, `content`, `published`, `published_at`, `created_at`, `updated_at`) VALUES
+(1, 'aled', 'led', 'ed', 0, NULL, NULL, NULL),
+(2, 'aled', 'led', 'ed', 0, NULL, NULL, NULL),
+(3, 'coucou', 'i ma a girou', 'yes adversaire tres fort ce soir\r\n## and i am alone\r\n- with me', 0, NULL, NULL, NULL);
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `articles`
+--
+ALTER TABLE `articles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `articles`
+--
+ALTER TABLE `articles`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
