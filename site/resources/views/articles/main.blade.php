da92376e5941923b587fe019e4a3959268ded7db
AledTahLesFous <clement.muzard@etu.umontpellier.fr>
2025-12-10 00:12:58 +0100

    feat: add a href for article id page

--- site/resources/views/articles/main.blade.php
+++ site/resources/views/articles/main.blade.php
@@ -1,5 +1,10 @@
 @foreach($articles as $article)
     <div class="article">
-        <h3>{{ $article->title }}</h3>
+        <h3>
+            <a href="{{ route('articles.show', $article->id) }}">
+                {{ $article->title }}
+            </a>
+        </h3>
+        <p>{{ $article->subtitle }}</p> <!-- si tu veux afficher le subtitle aussi -->
     </div>
 @endforeach
--- site/resources/views/layouts/app.blade.php
+++ site/resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $title ?? 'Mon Blog' }}</title>
+    <title>{{ $title ?? 'Le Blog "Les Connus"' }}</title>
 
     <style>
         body {
