5e63180fcae9b13511ffa096f4d0589107dabab4
AledTahLesFous <clement.muzard@etu.umontpellier.fr>
2025-12-10 13:53:48 +0100

    feat: debunk table with forgein key on article id

---
+++ site/database/migrations/2025_12_10_122420_create_debunk_articles.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+Schema::create('debunks', function (Blueprint $table) {
+    $table->id();
+    $table->string('article_id', 32)->unique(); // MD5 = 32 caractères
+    $table->string('title');
+    $table->string('subtitle')->nullable();
+    $table->text('content');
+    $table->timestamps();
+
+    $table->foreign('article_id')
+          ->references('id')
+          ->on('articles')
+          ->onDelete('cascade'); // supprime le debunk si l'article est supprimé
+});
+
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('debunks');
+    }
+};
