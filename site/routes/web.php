66c638b076edf772e06cf0d72a87a2a27de83dbc
AledTahLesFous <clement.muzard@etu.umontpellier.fr>
2025-12-10 13:54:26 +0100

    feat: debunk route

--- site/routes/web.php
+++ site/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\DebunkController;
 use App\Http\Controllers\NewsController;
 
 use App\Models\Article;
@@ -14,6 +15,8 @@ Route::get('/', function () {
 
 
 Route::get('/article/{id}', [ArticleController::class, 'show'])->name('articles.show'); // Page for article detail view
+Route::get('/articles/{articleId}/debunk', [DebunkController::class, 'show'])->name('debunk.show');
+
 
 Route::get('/articles/latest', function () {                        // api route for ajax update
     $articles = Article::orderBy('id', 'desc')->take(10)->get();
@@ -21,6 +24,11 @@ Route::get('/articles/latest', function () {                        // api route
 });
 
 
+
+
 Route::get('/api/articles/json', [NewsController::class, 'apiJson'])->name('news.api.json'); // API JSON route
 Route::get('/api/articles/one', [NewsController::class, 'apiOne']);
-Route::get('/api/articles/rewrite', [NewsController::class, 'apiEvalOne']);
+Route::get('/api/articles/rewrite', [NewsController::class, 'Main']);
+
+Route::get('/api/articles/debunk', [NewsController::class, 'apiDebunkOne']);
+
